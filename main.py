7805d2397d99612f27a02f399f75641f5f9abc89
Michi The Cat <mrzombsoriginal@gmail.com>
2026-03-01 15:39:58 +0300

    TODO #1

---
+++ main.py
@@ -0,0 +1,11 @@
+KEY = False     # Описание ниже | The description is below
+# Напишите KEY = False, если хотите, чтобы программа нашла ключ в C:\MyPythonFiles\GreenTeaBot\key
+# Напишите KEY = {ключ}, если хотите, чтобы программа не искала ключ в файлах
+# Write KEY = False if you want the program to find the key in C:\MyPythonFiles\GreenTeaBot\key
+# Write KEY = {key} if you want the program not to search for the key in the files
+
+# TODO прямо сейчас:
+# 1. импорт библиотек
+# 2. try/exec везде
+# 3. Система логов: нужна ли она тут, если инструменты не используются?
+# 4. создание остальной части кода и написание нового TODO
